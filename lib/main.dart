cd396f7c3811bb0a37b413d83c89129c48eec46e
Akbarre <akbarzainalapp@gmail.com>
2026-03-15 01:22:04 +0700

    saldo card

--- lib/main.dart
+++ lib/main.dart
@@ -87,4 +87,110 @@ bottomNavigationBar: BottomNavigationBar(
       ),
     );
   }
+
+
+  Widget _buildPremiumHeader() {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+
+        Container(
+          height: 150,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF00529C), Color(0xFF0074D9)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(48),
+              bottomRight: Radius.circular(40),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Column(
+                  crossAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Selamat Datang",
+                      style: TextStyle(color: Colors.white70, fontSize: 13),
+                    ),
+                    Text(
+                  "M Zainal Akbar",
+                  style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white24,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Text(
+                "Log Out",
+                style: TextStyle(color: Colors.white, fontSize: 10),
+              )
+            ),
+          ],
+        ),
+      ),
+    ),
+
+
+    positioned(
+      top: 90,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.88,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 15,
+            offset: const Offset(0, 8),
+          ),
+          ],
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Tabungan BRI BritAma",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13,
+                  ),
+                ),
+
+                
+              ]
+            )
+          ],
+        )
+      )
+    )
+
+  ),
+      ],
+    ),
+  }
 }
